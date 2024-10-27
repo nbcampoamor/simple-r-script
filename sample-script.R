f51833bde5f9260d2bd21aee50d7ffbbb561365c
Nicola Campoamor <nicola.campoamor@osumc.edu>
2024-10-27 14:59:54 -0400

    added code to sample-script

--- sample-script.R
+++ sample-script.R
@@ -7,6 +7,13 @@
 # Last Updated: [Replace with today's date]
 #--------------------------------------------------------------
 
+# I'm making a change to see if I am doing this right
+lapply(data, summary)
+
+git status
+git add sample-script.R
+git commit -m "added code to sample-script"
+
 # read in the sample data -------------------------------------
 data <- read.csv('data.csv')
 
